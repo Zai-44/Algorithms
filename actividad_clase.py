1f77cc7ffd9d0fd6ce65a43e903e167a4f7eebca
Zai-44 <zaira00.sand@gmail.com>
2025-03-10 07:47:22 -0600

    actividad_clase.py

---
+++ actividad_clase.py
@@ -0,0 +1,26 @@
+#Se asigna un valor a una variable
+numero = 10
+#Se imprime el valor de la varibale número, ej. 10
+print("El número asignado a la variable del numero es: ", numero)
+
+""" 
+Actividad de clase: van a definir valores de variables 
+y las van a imprimir; sobre algun hobby.
+Usarás:
+-Enteros
+-Booleanos
+-Caracteres
+Tiempo: 8 minutos
+ """
+
+ #Se asigna un valor a una variable
+años= 19 
+montar_caballo= True
+actividad_favorita= "montar caballos"
+nonmbre_del_caballo="Lucy"
+#Se imprime el valor de la varibale númeroj. 10
+print("Yo tengo: ", años, " años.","¿Me gustan los caballos?",montar_caballo,".De echo mi actividad favorita es: ",actividad_favorita," y mi caballo se llama: ",nonmbre_del_caballo)
+
+""" SENTENCIA IF -ES UNA CONDICIÓN SENTENCIA """
+if numero < (100):
+    print ("la variable número es menor que el 100")
