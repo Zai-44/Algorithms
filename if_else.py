76f9905a66c509f500c30c8476af92b3de92b46b
Zai-44 <zaira00.sand@gmail.com>
2025-03-10 09:53:30 -0600

    Sentencia If-else

---
+++ if_else.py
@@ -0,0 +1,14 @@
+""" SENTENCIA IF-ELSE """
+numero=50
+if numero <=20:
+    print("El número es menor o igual a 20")
+else:
+    print("El número es mayor que 20")
+
+""" Actividad: generar IF-ELSE al azar """
+
+numero=9
+if numero >= 5:
+    print(f"El número {numero} es mayor que 5")
+else:
+    print(f"El número {numero} es menor o igual a 5")
